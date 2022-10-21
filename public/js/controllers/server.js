fbd3b36f804366a58a068c2748f75483f821ef96
Amaroke <hugo.amaroke@gmail.com>
2022-10-21 11:12:15 +0200

    Changement de l'organisation des fichiers ajouts de l'index et gestion static des fichier par l'API

--- public/js/controllers/server.js
+++ public/js/controllers/server.js
@@ -2,12 +2,20 @@ const events = require('../model/events.js')
 const express = require('express')
 const app = express()
 
+// Racine du site
+app.use(express.static(__dirname+'/../../../public'));
+
+// Events
 app.get('/events', (req, res) => {
     res.send(events.showEvents())
 })
 
 app.get('/events/day', (req, res) => {
     res.send(events.showEventsDay(new Date(req.query.date)))
+})
+
+app.get('/events/week', (req, res) => {
+    res.send(events.showEventsWeek(new Date(req.query.date)))
 })
 
 app.get('/events/month', (req, res) => {
@@ -18,6 +26,7 @@ app.get('/events/year', (req, res) => {
     res.send(events.showEventsYear(new Date(req.query.date)))
 })
 
+// Server listen 8080
 app.listen(8080, () => {
     console.log("Server start !")
 })
--- public/js/model/events.js
+++ public/js/model/events.js
@@ -1,14 +1,14 @@
 // noinspection JSUnresolvedVariable
 
+const json = require("./events.json");
+
 module.exports = {
     showEvents: function () {
-        const json = require('./events.json')
         return JSON.stringify(json)
     },
     showEventsDay: function (date) {
-        const objEvents = require('./events.json')
         const events = []
-        objEvents.events.forEach(function (event) {
+        json.events.forEach(function (event) {
             const dateEvent = new Date(event.date)
             if (dateEvent.getDay() === date.getDay() && dateEvent.getMonth() === date.getMonth() && dateEvent.getFullYear() === date.getFullYear()) {
                 events.push(event)
@@ -16,10 +16,19 @@ module.exports = {
         })
         return events
     },
-    showEventsMonth: function (date) {
-        const objEvents = require('./events.json')
+    showEventsWeek: function (date) {
         const events = []
-        objEvents.events.forEach(function (event) {
+        json.events.forEach(function (event) {
+            const dateEvent = new Date(event.date)
+            if (dateEvent.getFullYear() === date.getFullYear() && getWeekNumber(dateEvent) === getWeekNumber(date)) {
+                events.push(event)
+            }
+        })
+        return events
+    },
+    showEventsMonth: function (date) {
+        const events = []
+        json.events.forEach(function (event) {
             const dateEvent = new Date(event.date)
             if (dateEvent.getMonth() === date.getMonth() && dateEvent.getFullYear() === date.getFullYear()) {
                 events.push(event)
@@ -28,9 +37,8 @@ module.exports = {
         return events
     },
     showEventsYear: function (date) {
-        const objEvents = require('./events.json')
         const events = []
-        objEvents.events.forEach(function (event) {
+        json.events.forEach(function (event) {
             const dateEvent = new Date(event.date)
             if (dateEvent.getFullYear() === date.getFullYear()) {
                 events.push(event)
@@ -38,4 +46,11 @@ module.exports = {
         })
         return events
     }
+}
+
+function getWeekNumber(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
 }
