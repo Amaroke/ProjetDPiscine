ac38b3b9d645349733beefbb23f9d5a64583b53c
Amaroke <hugo.amaroke@gmail.com>
2022-10-22 21:07:41 +0200

    Ajout de la gestion de codes HTTP et de nodemon

--- public/js/controllers/server.js
+++ public/js/controllers/server.js
@@ -4,59 +4,20 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const path = require("path");
 const app = express()
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 // Root
 app.use(express.static(__dirname + '/../../../public'));
 
-let urlEncodedParser = bodyParser.urlencoded({extended: false});
 
+// Login
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../views/register.html'));
+    // TODO CODE HTTP
+    res.sendFile(path.join(__dirname + '/../views/login.html'));
 })
-
-// Events
-app.get('/events', (req, res) => {
-    res.send(events.showEvents())
-})
-
-app.get('/events/day', (req, res) => {
-    res.send(events.showEventsDay(new Date(req.query.date)))
-})
-
-app.get('/events/week', (req, res) => {
-    res.send(events.showEventsWeek(new Date(req.query.date)))
-})
-
-app.get('/events/month', (req, res) => {
-    res.send(events.showEventsMonth(new Date(req.query.date)))
-})
-
-app.get('/events/year', (req, res) => {
-    res.send(events.showEventsYear(new Date(req.query.date)))
-})
-
-app.post('/events/add', urlEncodedParser, (req, res) => {
-    events.addEvent(req.body)
-    res.redirect("/");
-})
-
-// Attention, utiliser avec postman pour reset events.json
-app.post('/events/delete/all', (req, res) => {
-    events.deleteAll()
-    res.redirect("/")
-})
-
-app.post('/users/add', urlEncodedParser, (req, res) => {
-    let use = req.body.usernameRegister;
-    let pswd = req.body.passwordRegister;
-    let name = req.body.name;
-    let firstName = req.body.firstName;
-    users.addUser(use,pswd,name,firstName)
-    res.redirect("/login");
-})
-
 
 app.post('/login', urlEncodedParser, (req, res) => {
+    // TODO CODE HTTP
     let username = req.body.username;
     let password = req.body.password;
     if (users.verif(username, password) === true) {
@@ -66,6 +27,99 @@ app.post('/login', urlEncodedParser, (req, res) => {
         res.status(404)
     }
 });
+
+
+// Events
+app.get('/events', (req, res) => {
+    let eventsList = events.showEvents()
+    if(Object.keys(eventsList).length === 0) {
+        res.status(204)
+    } else {
+        res.status(200)
+    }
+    res.send(eventsList)
+})
+
+app.get('/events/day', (req, res) => {
+    let eventsList = events.showEventsDay(new Date(req.query.date))
+    if(Object.keys(eventsList).length === 0) {
+        res.status(204)
+    } else {
+        res.status(200)
+    }
+    res.send(eventsList)
+})
+
+app.get('/events/week', (req, res) => {
+    let eventsList = events.showEventsWeek(new Date(req.query.date))
+    if(Object.keys(eventsList).length === 0) {
+        res.status(204)
+    } else {
+        res.status(200)
+    }
+    res.send(eventsList)
+})
+
+app.get('/events/month', (req, res) => {
+    let eventsList = events.showEventsMonth(new Date(req.query.date))
+    if(Object.keys(eventsList).length === 0) {
+        res.status(204)
+    } else {
+        res.status(200)
+    }
+    res.send(eventsList)
+})
+
+app.get('/events/year', (req, res) => {
+    let eventsList = events.showEventsYear(new Date(req.query.date))
+    if(Object.keys(eventsList).length === 0) {
+        res.status(204)
+    } else {
+        res.status(200)
+    }
+    res.send(eventsList)
+})
+
+app.post('/events/add', urlEncodedParser, (req, res) => {
+    events.addEvent(req.body)
+    res.status(200)
+    res.redirect("/");
+})
+
+app.delete('/events/delete/all', (req, res) => {
+    events.deleteAll()
+    res.status(200)
+    res.redirect("/")
+})
+
+
+// Users
+app.post('/users/add', urlEncodedParser, (req, res) => {
+    //TODO CODE HTTP + PASSER BODY EN PARAM
+    let use = req.body.usernameRegister;
+    let pswd = req.body.passwordRegister;
+    let name = req.body.name;
+    let firstName = req.body.firstName;
+    users.addUser(use, pswd, name, firstName)
+    res.redirect("/login");
+})
+
+
+// Easter-eggs : https://developer.mozilla.org/fr/docs/Web/HTTP/Status/418
+app.get('/coffee', (req, res) => {
+    res.status(418)
+    res.send()
+})
+
+
+// Unknown endpoint
+app.use((req, res) => {
+    res.status(404);
+    res.json({
+        error: "Page not found"
+    });
+})
+
 
 // Server listen 8080
 app.listen(8080, () => {
