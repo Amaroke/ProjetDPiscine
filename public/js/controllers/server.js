f743490d52f22d22181ec79b4293317635af7bb5
Amaroke <hugo.amaroke@gmail.com>
2022-10-23 16:16:23 +0200

    Résolution du problème de login, utilisation de fetch

--- public/js/controllers/server.js
+++ public/js/controllers/server.js
@@ -3,13 +3,12 @@ const users = require('../model/users.js')
 const express = require('express')
 const bodyParser = require("body-parser")
 const path = require("path");
-const {existUser} = require("../model/users");
 const app = express()
-const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 // Root
 app.use(express.static(__dirname + '/../../../public'));
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
 
 // Login
 app.get('/login', (req, res) => {
@@ -17,20 +16,18 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname + '/../views/login.html'));
 })
 
-app.post('/login', urlEncodedParser, (req, res) => {
+app.post('/login', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
     if (users.existUser(username)) {
-        if (users.validUser(username, password) === true) {
+        if (users.validUser(username, password)) {
             res.status(200)
             res.redirect("/");
         } else {
-            res.status(401)
-            res.end("password")
+            res.status(401).send({error: "password"})
         }
     } else {
-        res.status(401)
-        res.end("username")
+        res.status(401).send({error: "username"})
     }
 });
 
@@ -86,7 +83,7 @@ app.get('/events/year', (req, res) => {
     res.send(eventsList)
 })
 
-app.post('/events/add', urlEncodedParser, (req, res) => {
+app.post('/events/add', (req, res) => {
     events.addEvent(req.body)
     res.status(200)
     res.redirect("/");
@@ -100,7 +97,7 @@ app.delete('/events/delete/all', (req, res) => {
 
 
 // Users
-app.post('/users/add', urlEncodedParser, (req, res) => {
+app.post('/users/add', (req, res) => {
     users.addUser(req.body)
     res.status(200)
     res.redirect("/login");
